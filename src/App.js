8e605653b455fc48f659815ffb9ebf92d4d17b9a
voramet <vorametz@gmail.com>
2021-11-04 10:11:45 +0700

    add omron-fins pkg

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import Paho from 'paho-mqtt'
 
-const client = new Paho.Client("broker.emqx.io", Number(8083), "asdas")
+var client = new Paho.Client("broker.emqx.io", Number(8083), "asdas")
+
+const omronFIN = (d) => {
+  const fins = require('omron-fins')
+  const options = { timeout: 5000, protocal: "udp" }
+  const ip = ''
+  const port = 9600
+  const client = fins.FinsClient(port, ip, options)
+
+  client.on('error',(error,msg) => {
+    console.log("Error: ",error,msg)
+  })
+
+  client.on('timeout',(host,msg) => {
+    console.log("Timeout: ",host,msg)
+  })
+
+  client.on('open',(info) => {
+    console.log("open: ",info)
+
+  })
+
+  client.connect()
+  const dAddress = ('00000' + d).substr(-5)
+  client.read(`D${dAddress}`, 1, (err, msg) => {
+    console.log("msg: ", msg)
+    console.log("err: ", err)
+  })
+}
 
 function App() {
   const [data, setData] = useState({})
-  const topics = ["mc1/status", 'mc2/status']
+  const [connection, setConnection] = useState("Disconnect")
+  const topics = ["mc1/status", 'mc2/status', 'dnth/energy/1', 'dnth/energy/2']
 
   client.onConnectionLost = function (responseObject) {
     console.log("Connection Lost: " + responseObject.errorMessage)
+    setConnection("Connection is lost")
   }
   client.onMessageArrived = function (message) {
     console.log(data)
     console.log("Message Arrived: " + message.payloadString)
     setData({ ...data, [message.destinationName]: message.payloadString })
   }
+
   function onConnect() {
-    console.log("Connected")
+    // console.log("Connected")
+    setConnection("Connected")
   }
-  useEffect(() => {
+
+  const connectMQTT = useCallback(() => {
+    client = new Paho.Client("broker.emqx.io", Number(8083), "asdas")
     client.connect({
       onSuccess: onConnect
     })
-    console.log("render")
   }, [])
+
+  useEffect(() => {
+    connectMQTT()
+    // console.log("render")
+  }, [connectMQTT])
 
   const publish = async (pubTopic) => {
     // const pubTopic = document.getElementById('topic_pub').value
@@ -44,6 +82,11 @@ function App() {
     console.log("subscribe: " + subTopic)
   }
 
+  const unsubscribe = (subTopic) => {
+    client.unsubscribe(subTopic)
+    console.log("unsubscribe: " + subTopic)
+  }
+
   const getCurrentData = () => {
     console.log(data)
   }
@@ -55,12 +98,15 @@ function App() {
         <script src="/paho-1.0.3/paho-mqtt.js"></script>
         <script src="mqttws31.js">
         </script>
+        <h2>Connection status: {connection}</h2>
+        <button onClick={connectMQTT}>Connect</button>
         <button onClick={getCurrentData}>Current Data</button>
         <table>
           <thead>
             <tr>
               <th>topic</th>
               <th>subscribe</th>
+              <th>unsubscribe</th>
               <th>publish</th>
               <th>msg</th>
             </tr>
@@ -73,7 +119,10 @@ function App() {
                   <button onClick={() => subscribe(topic)}>Sub</button>
                 </td>
                 <td>
-                  <input id={`msg-${topic}`}/>
+                  <button onClick={() => unsubscribe(topic)}>Unsub</button>
+                </td>
+                <td>
+                  <input id={`msg-${topic}`} />
                   <button onClick={() => publish(topic)}>Pub</button>
                 </td>
                 <td>{data[topic] || 'empty'}</td>
